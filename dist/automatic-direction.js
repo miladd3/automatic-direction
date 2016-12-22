0bff04bd9b3f61ccc4e0b308668c40015fc3fe97
Milad Dehghan <milad.d3@gmail.com>
2016-12-23 01:41:17 +0330

    Revert "Fixes #45"
    This reverts commit 51acfa28d5361b1a772ba4de93680b298dac9284.

--- dist/automatic-direction.js
+++ dist/automatic-direction.js
@@ -1,27 +1,20 @@
 /*!
- * automatic Direction v1.10
+ * automatic Direction v1.00
  * By Milad Dehghan (http://dehghan.net)
  * github: https://github.com/miladd3/automatic-direction
  */
-$(function() {	
-	
+$(function() {
 	$(".dir-auto").keyup(function(){
-
-		var textareavalue = $(this).val(); //Getting input value
-		var arabic = /[\u0600-\u06FF]/g; //setting arabic unicode
-		var match = textareavalue.match(arabic); 
-		var spacesMatch = textareavalue.match(new RegExp(" ", 'g'));
-		allcount = textareavalue.length;
-		farsicount = match ? match.length : 0;
-		spacesCount = spacesMatch ? spacesMatch.length : 0;
-		Englishcount = allcount - farsicount - spacesCount;
-
-    	if (farsicount > Englishcount) {
+		var textareavalue = $(this).val(); // Getting input value
+		var arabic = /[\u0600-\u06FF]/g; // setting arabic unicode
+		var match = textareavalue.match(arabic); // comparing with textbox value
+		allcount = textareavalue.length; // get all characters count
+		farsicount = match ? match.length : 0; // get arabic or farsi characters count
+		Englishcount = allcount - farsicount; // get English or latin characters count
+    	if (farsicount > Englishcount) { // comparing and setting html dir attribute
     		$(this).attr('dir','rtl');
     	}else {
     		$(this).attr('dir','ltr');
     	}
-    	
 	});
-
 });
--- docs/js/automatic-direction.js
+++ docs/js/automatic-direction.js
@@ -1,5 +1,5 @@
 /*!
- * automatic Direction v1.10
+ * automatic Direction v1.00
  * By Milad Dehghan (http://dehghan.net)
  * github: https://github.com/miladd3/automatic-direction
  */
@@ -10,21 +10,20 @@ $(function() {
 		var textareavalue = $(this).val(); //Getting input value
 		var arabic = /[\u0600-\u06FF]/g; //setting arabic unicode
 		var match = textareavalue.match(arabic); 
-		var spacesMatch = textareavalue.match(new RegExp(" ", 'g'));
 		allcount = textareavalue.length;
 		farsicount = match ? match.length : 0;
-		spacesCount = spacesMatch ? spacesMatch.length : 0;
-		Englishcount = allcount - farsicount - spacesCount;
-
+		Englishcount = allcount - farsicount;
     	if (farsicount > Englishcount) {
     		$(this).attr('dir','rtl');
     	}else {
     		$(this).attr('dir','ltr');
     	}
 
+
 		//Showing character count on front-end
     	$('.count-f').text(farsicount);
     	$('.count-e').text(Englishcount);
     	$('.count-all').text(allcount);
+
 	});
 });
