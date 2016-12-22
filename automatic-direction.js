d9e0d37f26f4a736ed2f5fb23a6d4c1e2c4628bb
Milad Dehghan <milad.d3@gmail.com>
2016-12-23 01:57:17 +0330

    Making project folder better

---
+++ automatic-direction.js
@@ -0,0 +1,27 @@
+/*!
+ * automatic Direction v1.10
+ * By Milad Dehghan (http://dehghan.net)
+ * github: https://github.com/miladd3/automatic-direction
+ */
+$(function() {	
+	
+	$(".dir-auto").keyup(function(){
+
+		var textareavalue = $(this).val(); //Getting input value
+		var arabic = /[\u0600-\u06FF]/g; //setting arabic unicode
+		var match = textareavalue.match(arabic); 
+		var spacesMatch = textareavalue.match(new RegExp(" ", 'g'));
+		allcount = textareavalue.length;
+		farsicount = match ? match.length : 0;
+		spacesCount = spacesMatch ? spacesMatch.length : 0;
+		Englishcount = allcount - farsicount - spacesCount;
+
+    	if (farsicount > Englishcount) {
+    		$(this).attr('dir','rtl');
+    	}else {
+    		$(this).attr('dir','ltr');
+    	}
+    	
+	});
+
+});
